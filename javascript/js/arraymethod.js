82beda6272bdf7be0e54c7ac7d17a950b172ab97
OnealTristan <tristanjulian37@gmail.com>
2021-03-11 15:21:41 +0700

    1.0
    javascript dan jquery

---
+++ javascript/js/arraymethod.js
@@ -0,0 +1,30 @@
+let nilai = [
+    { nama: "budi", ipa: 90, bahasa: 70, matematika: 80 },
+    { nama: "joni", ipa: 80, bahasa: 90, matematika: 60 },
+    { nama: "tejo", ipa: 75, bahasa: 80, matematika: 87 },
+    { nama: "siti", ipa: 90, bahasa: 85, matematika: 90 },
+];
+
+let nama = ["budi", "joni", "tejo", "siti"];
+// nama.push("ani", "roma");
+
+// console.log(nama.shift());
+
+// nama.unshift("bobi", "roki");
+
+// console.log(nama.slice(3, 6));
+
+let mapel = ['ipa', 'bahasa', 'matematika'];
+
+// console.log(nama.concat(mapel));
+
+console.log(nama.concat('ips', 'pkn', 'sejarah'));
+
+// console.log(nama.splice(3, 4));
+
+// console.log(nama.pop());
+
+
+// console.log(nilai[0].nama);
+// console.log(nama);
+
